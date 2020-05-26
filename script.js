5b05ebb8222adaf0f1478cbbd623e8531c04a251
Mardorf, Valerie <valerie.mardorf@cbsinteractive.com>
2020-05-25 22:37:19 -0700

    tweaks

--- script.js
+++ script.js
@@ -7,11 +7,9 @@ function docReady(fn) {
 };
 
 docReady(function() {
-  
   var x = window.matchMedia("(max-width: 768px)")
   changeLayout(x) 
   x.addListener(changeLayout)
-  
   function changeLayout(x) {
     if (x.matches) { 
       document.getElementById("AppLayout").classList.add("mobile");
@@ -24,34 +22,32 @@ docReady(function() {
      document.getElementById("AppNav").style.marginLeft = "0";
     }
   };
-  
-  function toggleDrawer() {
-    var x = document.getElementById("AppDrawer");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-      document.getElementById("AppDrawer").style.width = "0";
-      document.getElementById("AppPage").style.marginLeft = "0";
-      document.getElementById("AppNav").style.marginLeft = "0";
-    } else {
-      x.style.display = "flex";
-      document.getElementById("AppDrawer").style.width = "20.8rem";
-      document.getElementById("AppPage").style.marginLeft = "20.8rem";
-      document.getElementById("AppNav").style.marginLeft = "20.8rem";
-      document.getElementById("AppSearch").style.display = "none";
-    }
-  }
-
-  document.querySelector("#SearchOpen").addEventListener('click', function() {
-      document.querySelector("#AppOverlay").style.display = 'block';
-      document.querySelector("body").style.overflow = 'hidden';
-  });
-
-  document.querySelector("#SearchClose").addEventListener('click', function() {
-      document.querySelector("#AppOverlay").style.display = 'none';
-      document.querySelector("body").style.overflow = 'visible';
-  });
-  
 });
 
+function toggleDrawer() {
+  var x = document.getElementById("AppDrawer");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    document.getElementById("AppDrawer").style.width = "0";
+    document.getElementById("AppPage").style.marginLeft = "0";
+    document.getElementById("AppNav").style.marginLeft = "0";
+  } else {
+    x.style.display = "flex";
+    document.getElementById("AppDrawer").style.width = "20.8rem";
+    document.getElementById("AppPage").style.marginLeft = "20.8rem";
+    document.getElementById("AppNav").style.marginLeft = "20.8rem";
+  }
+};
+
+window.onload=function(){
+  document.querySelector("#SearchOpen").addEventListener("click", function() {
+    document.querySelector("#AppOverlay").style.display = "block";
+    document.querySelector("body").style.overflow = "hidden";
+  });
+  document.querySelector("#SearchClose").addEventListener("click", function() {
+    document.querySelector("#AppOverlay").style.display = "none";
+    document.querySelector("body").style.overflow = "visible";
+  });
+}
 
 // https://abouolia.github.io/sticky-sidebar/
